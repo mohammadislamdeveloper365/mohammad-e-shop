71932d6e62832880802ee6e4b2e84e66b97a807d
Mohammad Moinul Islam <95491483+mohammadislamdeveloper365@users.noreply.github.com>
2023-08-17 19:37:05 +0400

    add functionality to cart

--- src/OrderPage.jsx
+++ src/OrderPage.jsx
@@ -1,15 +1,18 @@
 import { useLoaderData } from 'react-router-dom'
 import ReviewItem from './components/review-item/ReviewItem';
 import ShoppingSummary from './components/shopping-summary/ShoppingSummary'
+import { deleteFromDb } from './utilities/localStorage.js'
 import './OrderPage.css'
+import { useState } from 'react';
 
 export default function OrderPage() {
   const [,cart] = useLoaderData();
+  const [reviewCart, setReviewCart] = useState(cart);
   return (
     <div className='mohammad-e-shop-order'>
       <div className='mohammad-e-shop-order-cart'>
         {
-          cart.map(product=><ReviewItem shoppingItem={product} key={product.key}/>)
+          reviewCart.map(product=><ReviewItem shoppingItem={product} key={product.key} handleButtonClick={()=>removeItemFromCart(product.key)}/>)
         }
       </div>
       <div className='mohammad-e-shop-order-summary'>
@@ -17,4 +20,10 @@ export default function OrderPage() {
       </div>
     </div>
   )
+
+  function removeItemFromCart(id) {
+    const remainingCart = reviewCart.filter(product=> id !== product.key );
+    setReviewCart(remainingCart);
+    deleteFromDb(id);
+  }
 }
--- src/components/review-item/ReviewItem.jsx
+++ src/components/review-item/ReviewItem.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
+import Button from '../core/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import './ReviewItem.css'
 
 export default function ReviewItem(props) {
-    const { shoppingItem } = props;
+    const { shoppingItem, handleButtonClick } = props;
   return (
     <div className='mohammad-e-shop-review-item-container'>
       <div className='mohammad-e-shop-review-item-details'>
@@ -12,10 +15,13 @@ export default function ReviewItem(props) {
                 <h5 className='mohammad-e-shop-review-item-description'>Price: {shoppingItem.wholePrice}</h5>
                 <p className='mohammad-e-shop-review-item-description'>Manufacturer: {shoppingItem.seller}</p>
                 <p className='mohammad-e-shop-review-item-description'>Rating: {shoppingItem.star}</p>
+                <p className='mohammad-e-shop-review-item-description'>Quantity: {shoppingItem.items}</p>
             </div>
       </div>
-      <div className='mohammad-e-shop-review-item-btn'>
-            
+      <div className='mohammad-e-shop-review-item-btn-container mohammad-e-shop-review-item-description'>
+            <Button className='mohammad-e-shop-review-item-btn' handleButtonClick={handleButtonClick}>
+              <FontAwesomeIcon icon={faTrashAlt} className='mohammad-e-shop-review-item-delete-btn'/>
+            </Button>
       </div>
     </div>
   )
@@ -24,5 +30,6 @@ export default function ReviewItem(props) {
 ReviewItem.propTypes = {
     shoppingItem : PropTypes.object,
     className: PropTypes.string,
+    handleButtonClick: PropTypes.func
     
 }
